89935584231200bea62165b8c03650a1b0f0fe16
KyroDevz <kyrodevz@hotmail.com>
2025-04-19 19:47:59 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 import authRoutes from "./routes/auth";
 
 dotenv.config();
@@ -8,6 +11,16 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use("/", authRoutes);
+app.use(cors());
+app.use(express.json)
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const workspaceRoutes = require('./routes/workspaces');
+app.use('/api/workspaces', workspaceRoutes);
 
 app.listen(PORT, () => {
   console.log(`Backend listening on port ${PORT}`);
